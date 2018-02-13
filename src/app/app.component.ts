ad82c2a7548833ffa9c72fd18f24bb48ae95dbd9
Saenen <hans.saenen@wolterskluwer.com>
2018-02-13 14:08:28 +0100

    style test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'app';
+  tooltipOptions: any;
+
+  ngOnInit() {
+    this.tooltipOptions = {
+      title: 'I am wk-ui Tooltip, buddy',
+      position: 'right'
+    };
+  }
 }
+
+
